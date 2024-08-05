6fb4c5db4b62828760bb5c364b85c35e8385c2ed
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2024-08-05 10:17:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from langchain.chains.summarize import load_summarize_chain
 
 def generate_response(txt):
     # Instantiate the LLM model
-    llm = OpenAI(temperature=0, openai_api_key=OpenAI API Key)
+    llm = OpenAI(temperature=0, openai_api_key= OpenAI API Key)
     # Split text
     text_splitter = CharacterTextSplitter()
     texts = text_splitter.split_text(txt)
